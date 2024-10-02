7354507f32f944618ffd4dc2192387992d6cf455
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-10-02 11:26:16 +0200

    New exercice

---
+++ data/users.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'username' => 'admin',
+        'firstName' => 'Céline',
+        'roles' => ['ROLE_ADMIN', 'ROLE_USER'],
+        'createdAt' => '2019-01-01 00:00:00',
+    ],
+    [
+        'id' => 2,
+        'username' => 'mango',
+        'firstName' => 'Mangolarkalamerrosh',
+        'roles' => ['ROLE_USER'],
+        'createdAt' => '2019-01-01 00:00:00',
+    ],
+    [
+        'id' => 3,
+        'username' => 'lukka',
+        'firstName' => 'Lucas',
+        'roles' => ['ROLE_MANAGER'],
+        'createdAt' => '2019-01-01 00:00:00',
+    ],
+];
