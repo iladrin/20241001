1f0f1ab88fcda2a8e989acf74d0e501e3dbe8e36
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-10-03 17:29:46 +0200

    Demo about logs

--- demo.php
+++ demo.php
@@ -1,5 +1,47 @@
 <?php
 
-if (!isset($x)) {
-    $x = 5;
+class Registry
+{
+    private array $items = [];
+
+    public function register(mixed $item): void
+    {
+        if (in_array($item, $this->items, true)) {
+            return;
+        }
+
+        $this->items[] = $item;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
 }
+
+class Logger
+{
+
+    public function __construct(
+        private Registry $registry
+    )
+    {
+        $this->registry->register($this);
+    }
+
+    public function log(string $level, string $message): void
+    {
+        $message = date('c') . " : $level - $message" . PHP_EOL;
+        file_put_contents(__DIR__ . '/logs/log.txt', $message, FILE_APPEND);
+    }
+}
+
+$registry = new Registry();
+
+$logger = new Logger($registry);
+$logger->log('info', 'Hello world');
+
+$logger = new Logger($registry);
+$logger->log('info', 'Hello world');
+
+var_dump($registry->getItems());
