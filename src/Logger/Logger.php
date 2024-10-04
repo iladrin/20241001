9baa52f2613b473774bf29a370c3bd727c16b2e3
Benoit Tavernier <benoit.tavernier@gmail.com>
2024-10-04 17:16:32 +0200

    Use Psr Log

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -2,13 +2,16 @@
 
 namespace App\Logger;
 
-class Logger implements LoggerInterface
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerInterface;
+
+class Logger extends AbstractLogger implements LoggerInterface
 {
     public function __construct(private readonly string $fileName)
     {
     }
 
-    public function log(string $level, string $message): void
+    public function log($level, string|\Stringable $message, array $context = []): void
     {
         $logMessage = sprintf('[%s] %s: %s', date('Y-m-d H:i:s'), $level, $message) . PHP_EOL;
         file_put_contents($this->fileName, $logMessage, FILE_APPEND);
--- src/Logger/LoggerAwareTrait.php
+++ src/Logger/LoggerAwareTrait.php
@@ -2,11 +2,16 @@
 
 namespace App\Logger;
 
+// Il existe déjà un LoggerInterface dans ce `namespace`,
+// on peut soit l'écraser par le `use` suivant (sans alias),
+// soit utiliser l'alias pour conserver les 2 symboles accessibles
+use \Psr\Log\LoggerInterface as PsrLoggerInterface;
+
 trait LoggerAwareTrait
 {
-    protected readonly LoggerInterface $logger;
+    protected readonly PsrLoggerInterface $logger;
 
-    public function setLogger(LoggerInterface $logger): static
+    public function setLogger(PsrLoggerInterface $logger): static
     {
         $this->logger = $logger;
         return $this;
